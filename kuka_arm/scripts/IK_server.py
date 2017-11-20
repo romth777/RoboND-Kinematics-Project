f940ba677df036925224b25a6bc55bb4b3e97659
romth777 <roomtheater@gmail.com>
2017-11-20 04:55:04 -0800

    update

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -28,20 +28,42 @@ def handle_calculate_IK(req):
 		
         ### Your FK code here
         # Create symbols
-	#
-	#   
-	# Create Modified DH parameters
-	#
-	#            
-	# Define Modified DH Transformation matrix
-	#
-	#
-	# Create individual transformation matrices
-	#
-	#
-	# Extract rotation matrices from the transformation matrices
-	#
-	#
+        q1, q2, q3, q4, q5, q6, q7 = symbols('q1:8')
+        d1, d2, d3, d4, d5, d6, d7 = symbols('d1:8')
+        a0, a1, a2, a3, a4, a5, a6 = symbols('a0:7')
+        alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha0:7')
+
+	    # Create Modified DH parameters
+	    s = {alpha0:    0,  a0:     0,  d1:  0.75,
+             alpha1:-pi/2,  a1:  0.35,  d2:     0,  q2: q2-pi/2,
+             alpha2:    0,  a2:  1.25,  d3:     0,
+             alpha3:-pi/2,  a3:-0.054,  d4:  1.50,
+             alpha4: pi/2,  a4:     0,  d5:     0,
+             alpha5:-pi/2,  a5:     0,  d6:     0,
+             alpha6:    0,  a6:     0,  d7: 0.303,  q7:     0}
+
+	    # Define Modified DH Transformation matrix
+        def TF_Matrix(alpha, a, d, q):
+            TF = Matrix([[cos(q), -sin(q), 0, a],
+                         [sin(q) * cos(alpha), cos(q) * cos(alpha), -sin(alpha), -sin(alpha) * d],
+                         [sin(q) * sin(alpha), cos(q) * sin(alpha), cos(alpha), cos(alpha) * d],
+                         [0, 0, 0, 1]])
+            return TF
+
+	    # Create individual transformation matrices
+        T0_1 = TF_Matrix(alpha0, a0, d1, q1).subs(s)
+        T1_2 = TF_Matrix(alpha1, a1, d2, q2).subs(s)
+        T2_3 = TF_Matrix(alpha2, a2, d3, q3).subs(s)
+        T3_4 = TF_Matrix(alpha3, a3, d4, q4).subs(s)
+        T4_5 = TF_Matrix(alpha4, a4, d5, q5).subs(s)
+        T5_6 = TF_Matrix(alpha5, a5, d6, q6).subs(s)
+        T6_G = TF_Matrix(alpha6, a6, d7, q7).subs(s)
+
+        T0_G = T0_1 * T1_2 * T2_3 * T3_4 * T4_5 * T5_6 * T6_G
+
+    	# Extract rotation matrices from the transformation matrices
+
+
         ###
 
         # Initialize service response
@@ -50,9 +72,9 @@ def handle_calculate_IK(req):
             # IK code starts here
             joint_trajectory_point = JointTrajectoryPoint()
 
-	    # Extract end-effector position and orientation from request
-	    # px,py,pz = end-effector position
-	    # roll, pitch, yaw = end-effector orientation
+	        # Extract end-effector position and orientation from request
+	        # px,py,pz = end-effector position
+	        # roll, pitch, yaw = end-effector orientation
             px = req.poses[x].position.x
             py = req.poses[x].position.y
             pz = req.poses[x].position.z
@@ -62,18 +84,69 @@ def handle_calculate_IK(req):
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
      
             ### Your IK code here 
-	    # Compensate for rotation discrepancy between DH parameters and Gazebo
-	    #
-	    #
-	    # Calculate joint angles using Geometric IK method
-	    #
-	    #
+	        # Compensate for rotation discrepancy between DH parameters and Gazebo
+            r, p, y = symbols('r p y')
+
+            R_x = Matrix([[1, 0, 0],
+                          [0, cos(r), -sin(r)],
+                          [0, sin(r), cos(r)]])
+            R_y = Matrix([[cos(p), 0, sin(p)],
+                          [0, 1, 0],
+                          [-sin(p), 0, cos(p)]])
+            R_z = Matrix([[cos(y), -sin(y), 0],
+                          [sin(y), cos(y), 0],
+                          [0, 0, 1]])
+            R_G = R_z * R_y * R_x
+
+            R_Err = R_z.subs(y, radians(180)) * R_y.subs(p, radians(-90))
+            R_G = R_G * R_Err
+            R_G = R_G.subs({'r':roll, 'p':pitch, 'y':yaw})
+
+            G = Matrix([[px],
+                        [py],
+                        [pz]])
+            WC = G - (0.303) * R_G[:,2]
+
+    	    # Calculate joint angles using Geometric IK method
+            theta1 = atan2(WC[1], WC[0])
+
+            side_a = 1.501
+            side_b = sqrt(pow((sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35), 2) + pow((WC[2] - 0.75), 2))
+            side_c = 1.25
+
+            angle_a = acos((side_b * side_b + side_c * side_c - side_a * side_a) / (2 * side_b * side_c))
+            angle_b = acos((side_a * side_a + side_c * side_c - side_b * side_b) / (2 * side_a * side_c))
+
+            theta2 = pi/2 - angle_a - atan2(WC[2] - 0.75, sqrt(WC[0] * WC[0] + WC[1] * WC[1]) - 0.35)
+
+            theta3 = pi/2 - (angle_b + 0.036)
+
+            R0_3 = T0_1[0:3,0:3] * T1_2[0:3,0:3] * T2_3[0:3,0:3]
+            R0_3 = R0_3.evalf(subs={q1:theta1, q2:theta2, q3:theta3})
+
+            R3_6 = R0_3.inv("LU") * R_G
+
+            theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+            theta5 = atan2(-R3_6[1,1]/sin(theta6), R3_6[1,2])
+            theta4 = atan2(R3_6[2,2]/sin(theta5), -R3_6[0,2]/sin(theta5))
+
+            if x >= len(req.poses):
+                theta5 = theta5_fixed
+                theta6 = theta6_fixed
+
+            theta1_fixed = theta1
+            theta2_fixed = theta2
+            theta3_fixed = theta3
+            theta4_fixed = theta4
+            theta5_fixed = theta5
+            theta6_fixed = theta6
+
             ###
 		
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
-	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
-	    joint_trajectory_list.append(joint_trajectory_point)
+	        joint_trajectory_point.positions = [theta1_fixed, theta2_fixed, theta3_fixed, theta4_fixed, theta5_fixed, theta6_fixed]
+	        joint_trajectory_list.append(joint_trajectory_point)
 
         rospy.loginfo("length of Joint Trajectory List: %s" % len(joint_trajectory_list))
         return CalculateIKResponse(joint_trajectory_list)
